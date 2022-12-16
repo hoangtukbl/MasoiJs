6ca6ed0c9bc28efde46eb4f3c55e00eb8c409691
Bin-08-01 <minhtri192035@gmail.com>
2022-12-16 23:28:24 +0700

    Update __init__.js

--- role/__init__.js
+++ role/__init__.js
@@ -1,17 +1,18 @@
 const Bodyguard = require('./bodyguard');
 const Seer = require('./seer');
 const Villager = require('./villagers');
+const Witch = require('./witch');
 const Wolf = require('./wolf');
-
 
 class Init {
     #listAttend = [];
-    #listRole = ["bodyguard", "phuthuy", "soi1", "soi2", "dan1", "dan2",
-        "banlang", "hunter", "truonglang", "soi3", "boi", "dan3"];
+    #listRole = ["bodyguard", "witch", "wolf", "wolf", "village", "village",
+        "cursed", "hunter", "mayor", "wolf", "diviner", "village"];
     #listPlayer = [];
 
     constructor(listPlayer) {
         this.setListPlayer(listPlayer);
+        this.setRole();
     }
 
     setListPlayer(listAttend) {
@@ -19,21 +20,49 @@ class Init {
     }
 
     getListPlayer() {
+        this.#listPlayer.forEach(each => {
+            console.log(each.getName(),
+                each.getId(),
+                each.getLegit(),
+                each.getRole(),
+                each.getState())
+        })
         return this.#listPlayer;
     }
 
     setRole() {
-        const temp = [];
-        for (let i = 0; i < this.#listAttend.length; ++i) {
-            temp.push(i);
-        }
-        this.#listAttend.forEach((each) => {
-            let key = Math.floor(Math.random() * this.#listAttend.length);
-            temp.splice(key, 1);
-            if (this.#listRole[key] === 'bodyguard') {
-                this.#listPlayer.push(new Bodyguard(each.name, each.id));
-            } else {
-                this.#listPlayer.push(new Villager(each.name, each.id));
+        this.#listAttend.sort(() => Math.random() - 0.5);
+        this.#listAttend.forEach((each, key) => {
+            switch (this.#listRole[key]) {
+                case 'village':
+                    this.#listPlayer.push(new Villager(each.name, each.id));
+                    break;
+                case "bodyguard":
+                    this.#listPlayer.push(new Bodyguard(each.name, each.id));
+                    break;
+                case 'wolf':
+                    this.#listPlayer.push(new Wolf(each.name, each.id));
+                    break;
+                case 'seer':
+                    this.#listPlayer.push(new Seer(each.name, each.id));
+                    break;
+                case 'witch':
+                    this.#listPlayer.push(new Witch(each.name, each.id));
+                    break;
+            }
+        })
+    }
+
+    getPlayerLife() {
+        const data = this.#listPlayer.filter(each => each.getState())
+        return data;
+    }
+
+    handleKill(idPlayer){
+        this.#listPlayer.forEach(each => {
+            const t = each.getId();
+            if(t == idPlayer){
+                each.setState(false);
             }
         })
     }
