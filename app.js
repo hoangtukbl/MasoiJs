d5fe7641be6625eaa5f9fb5465f9a66e937cb8b9
Bin-08-01 <minhtri192035@gmail.com>
2022-12-16 23:28:07 +0700

    Test

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const Game = require('./role/__init__');
 
-const listPlayer = [{name: 'Tri', role: '', id: '1'}, {name: 'Tu', role: '', id: '2'},
-    {name: 'Thong', role: '', id: '3'}, {name: 'Dung', role: '', id: '4'},{name: 'Huy', role: '', id: '5'}, {name: 'Mai', role: '', id: '6'}];
-const test = new Game(listPlayer);
+const listPlayer = [
+    {name: 'Tri', role: '', id: '1'},
+    {name: 'Tu', role: '', id: '2'},
+    {name: 'Thong', role: '', id: '3'},
+    {name: 'Dung', role: '', id: '4'},
+    {name: 'Huy', role: '', id: '5'},
+    {name: 'Mai', role: '', id: '6'}
+];
 
-console.log(test.getListPlayer());
-test.setRole();
-console.log(test.getListPlayer());
+const game = new Game(listPlayer);
+
+const t = game.getListPlayer();
+console.log(t);
+t[2].kill(2);
+console.log(t[2].getTargetKill());
+console.log(game.getPlayerLife());
+
+game.handleKill(t[2].getTargetKill());
+console.log(game.getPlayerLife());
