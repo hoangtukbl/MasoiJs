20667ef388929148db4ce226bc8e4462a38cf23f
Bin-08-01 <minhtri192035@gmail.com>
2022-12-16 23:28:53 +0700

    Update wolf.js

--- role/wolf.js
+++ role/wolf.js
@@ -1,14 +1,19 @@
 const Player = require('./player');
 
 class Wolf extends Player {
+    #targetKill;
     constructor(name, id) {
         super(name, id);
         this.setRole('wolf');
         this.setLegit(false);
     }
 
-    kill(){
+    kill(id12Kill){
+        this.#targetKill = id12Kill;
+    }
 
+    getTargetKill(){
+        return this.#targetKill;
     }
 }
 
