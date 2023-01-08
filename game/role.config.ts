229efc5d88fc0d70d3ebfd668baf22647b0e1f6e
Bin-08-01 <minhtri192035@gmail.com>
2023-01-08 16:58:26 +0700

    Create role.config.ts

---
+++ game/role.config.ts
@@ -0,0 +1,74 @@
+export const RoleConfig = [
+    {
+        name: "bodyguard",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "witch",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "wolf",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "wolf",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "village",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "village",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "cursed",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "hunter",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "mayor",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "wolf",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "diviner",
+        description: "",
+        legit: true,
+
+    },
+    {
+        name: "village",
+        description: "",
+        legit: true,
+
+    }
+]
